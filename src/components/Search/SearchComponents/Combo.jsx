d4e0a961b93c1094001c1a3182c8ac10b20ed212
fulail-kt <muhamedfulail77@gmail.com>
2024-07-28 22:40:02 +0530

    filter

--- src/components/Search/SearchComponents/Combo.jsx
+++ src/components/Search/SearchComponents/Combo.jsx
@@ -4,6 +4,7 @@
 // import flightLogo from "../../../assets/home/logo/image 40.png"
 // import ComboSideBar from "./ComboSidebar";
 
+
 // const { TabPane } = Tabs;
 
 // const Combo = ({ flightprops }) => {
@@ -40,7 +41,7 @@
 
 //   const [filteredFlights, setFilteredFlights] = useState(flightprops);
 //   const [filters, setFilters] = useState({
-//     maxPrice: maxPrice,
+//     maxPrice: maxPrice, 
 //     stops: [],
 //     departureTime: [],
 //     arrivalTime: [],
@@ -54,7 +55,7 @@
 //       const departureHour = new Date(flight.sI[0].dt).getHours();
 //       const arrivalHour = new Date(flight.sI[flight.sI.length - 1].at).getHours();
 //       const airline = flight.sI[0].fD.aI.name;
-
+  
 //       const priceMatch = price <= filters.maxPrice;
 //       const stopsMatch = filters.stops.length === 0 || filters.stops.includes(stops.toString());
 //       const departureMatch = filters.departureTime.length === 0 || filters.departureTime.some(range => {
@@ -66,17 +67,19 @@
 //         return arrivalHour >= start && arrivalHour < end;
 //       });
 //       const airlineMatch = filters.airlines.length === 0 || filters.airlines.includes(airline);
-
+  
 //       return priceMatch && stopsMatch && departureMatch && arrivalMatch && airlineMatch;
 //     });
-
+  
 //     setFilteredFlights(newFilteredFlights);
 //   }, [filters, flightprops]);
 
 //   const startSegment = filteredFlights[0]?.sI[0];
 //   const endSegment = filteredFlights[0]?.sI[filteredFlights[0]?.sI.length - 1];
 
+
 //   console.log(flightprops,"pros")
+
 
 //   return (
 //     <div className="flex flex-row h-screen">
@@ -113,10 +116,11 @@
 
 // export default Combo;
 
+
 import React, { useEffect, useState } from "react";
 import { Tabs, Button } from "antd";
 import ComboFlightCard from "../Cards/ComboFlightCard";
-import flightLogo from "../../../assets/home/logo/image 40.png";
+import flightLogo from "../../../assets/home/logo/image 40.png"
 import ComboSideBar from "./ComboSidebar";
 
 const { TabPane } = Tabs;
@@ -127,92 +131,71 @@ const Combo = ({ flightprops }) => {
   }
 
   const findMaxPrice = (flights) => {
-    return Math.max(
-      ...flights.map((flight) => flight.totalPriceList[0].fd.ADULT.fC.TF)
-    );
+    return Math.max(...flights.map(flight => flight.totalPriceList[0].fd.ADULT.fC.TF));
   };
 
   const maxPrice = findMaxPrice(flightprops);
 
+
   const [filteredFlights, setFilteredFlights] = useState(flightprops);
   const [filters, setFilters] = useState({
-    maxPrice: maxPrice,
+    maxPrice: maxPrice, 
     stops: [],
     departureTime: [],
     arrivalTime: [],
-    airlines: [],
+    airlines: []
   });
 
   useEffect(() => {
-    const newFilteredFlights = flightprops.filter((flight) => {
+    const newFilteredFlights = flightprops.filter(flight => {
       const price = flight.totalPriceList[0].fd.ADULT.fC.TF;
       const stops = flight.sI[0].stops;
       const departureHour = new Date(flight.sI[0].dt).getHours();
-      const arrivalHour = new Date(
-        flight.sI[flight.sI.length - 1].at
-      ).getHours();
+      const arrivalHour = new Date(flight.sI[flight.sI.length - 1].at).getHours();
       const airline = flight.sI[0].fD.aI.name;
-
+  
       const priceMatch = price <= filters.maxPrice;
-      const stopsMatch =
-        filters.stops.length === 0 || filters.stops.includes(stops.toString());
-      const departureMatch =
-        filters.departureTime.length === 0 ||
-        filters.departureTime.some((range) => {
-          const [start, end] = range.split("-").map(Number);
-          return departureHour >= start && departureHour < end;
-        });
-      const arrivalMatch =
-        filters.arrivalTime.length === 0 ||
-        filters.arrivalTime.some((range) => {
-          const [start, end] = range.split("-").map(Number);
-          return arrivalHour >= start && arrivalHour < end;
-        });
-      const airlineMatch =
-        filters.airlines.length === 0 || filters.airlines.includes(airline);
-
-      return (
-        priceMatch &&
-        stopsMatch &&
-        departureMatch &&
-        arrivalMatch &&
-        airlineMatch
-      );
+      const stopsMatch = filters.stops.length === 0 || filters.stops.includes(stops.toString());
+      const departureMatch = filters.departureTime.length === 0 || filters.departureTime.some(range => {
+        const [start, end] = range.split('-').map(Number);
+        return departureHour >= start && departureHour < end;
+      });
+      const arrivalMatch = filters.arrivalTime.length === 0 || filters.arrivalTime.some(range => {
+        const [start, end] = range.split('-').map(Number);
+        return arrivalHour >= start && arrivalHour < end;
+      });
+      const airlineMatch = filters.airlines.length === 0 || filters.airlines.includes(airline);
+  
+      return priceMatch && stopsMatch && departureMatch && arrivalMatch && airlineMatch;
     });
-
+  
     setFilteredFlights(newFilteredFlights);
   }, [filters, flightprops]);
 
+
+  const handleBooking = (flightIndex, priceIndex) => {
+    const selectedFlight = filteredFlights[flightIndex];
+    const selectedPrice = selectedFlight.totalPriceList[priceIndex];
+    const priceId = selectedPrice.id;
+    console.log("Selected price ID:", priceId);
+  };
+
   return (
-    <div className="flex flex-row h-screen">
-      <ComboSideBar
-        filters={filters}
-        setFilters={setFilters}
-        flights={flightprops}
-        maxPrice={maxPrice}
-      />
+    <div className="flex flex-col md:flex-row md:h-screen">
+      <ComboSideBar filters={filters} setFilters={setFilters} flights={flightprops} maxPrice={maxPrice}/>
       <div className="flex-1 overflow-y-auto">
         <Tabs defaultActiveKey="1">
           <TabPane tab="All Combo flights" key="1">
-            {filteredFlights.map((flight, index) => (
-              <div
-                key={index}
-                className="border shadow-xl p-4 mb-4 rounded-lg relative"
-              >
-                {/* {flight.map((flig, segIndex) => ( */}
-                <ComboFlightCard
-                  key={index}
-                  logo={flightLogo}
-                  flightDetails={flight}
-                />
-                {/* ))} */}
-                <div className="flex justify-start">
-                  <Button className="py-4 m-2" type="primary">
-                    Book now
-                  </Button>
-                </div>
-              </div>
-            ))}
+          {filteredFlights.map((flight, index) => (
+        <div key={index} className="border shadow-xl p-4 mb-4 rounded-lg relative">
+          <ComboFlightCard 
+            key={index} 
+            logo={flightLogo} 
+            flightDetails={flight} 
+            onBooking={(priceIndex) => handleBooking(index, priceIndex)}
+          />
+        </div>
+      ))}
           </TabPane>
         </Tabs>
       </div>
