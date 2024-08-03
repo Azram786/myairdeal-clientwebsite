6cac2b4b8b97b023b2d25f87c7eaf17e0d9bd947
Naren <deadlyassassin2003@gmail.com>
2024-08-03 17:01:14 +0530

    adding service component

---
+++ src/components/Service/content.jsx
@@ -0,0 +1,30 @@
+import plane from "../../assets/service/plane.png"
+
+const Content = [
+  {
+    id: 1,
+    imgURL: plane,
+    head: "Helicopter",
+    para: "Engrossed listening. Park gate sell they west hard for the.",
+  },
+  {
+    id: 2,
+    imgURL: plane,
+    head: "Helicopter",
+    para: "Engrossed listening. Park gate sell they west hard for the.",
+  },
+  {
+    id: 3,
+    imgURL: plane,
+    head: "Helicopter",
+    para: "Engrossed listening. Park gate sell they west hard for the.",
+  },
+  {
+    id: 4,
+    imgURL: plane,
+    head: "Helicopter",
+    para: "Engrossed listening. Park gate sell they west hard for the.",
+  }
+];
+
+export default Content;
