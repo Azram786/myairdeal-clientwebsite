2c2fbc5947f679f3364970c6dc9b25a4a4a2e067
swarna856 <swarnaav82@gmail.com>
2024-08-27 16:40:31 +0530

    no changes

--- src/components/Home/ResentSearch.jsx
+++ src/components/Home/ResentSearch.jsx
@@ -74,7 +74,7 @@ const RecentSearch = ({ ResentSearchData }) => {
             >
               <div className="flex flex-col space-y-2">
                 <div className="flex flex-row justify-between items-center">
-                  <div className="text-left w-[40%]">
+                  <div className="text-left w-[50%] ">
                     <div className="text-xs font-bold">
                       {search?.searchQuery.routeInfos[0].fromCityOrAirport
                         ?.code || "N/A"}
